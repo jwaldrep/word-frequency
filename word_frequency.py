f223928110405d81ca77a146bc765ec8edd0619f
John Waldrep <johnwaldrep@gmail.com>
2015-05-06 14:09:34 -0400

    Add option to ignore common words

--- word_frequency.py
+++ word_frequency.py
@@ -1,7 +1,19 @@
-
+"""Improvements:
+    don't count we're as were
+    figure out how to cleanly import COMMON_WORDS
+"""
 from pprint import pprint as pprint
 import re
 
+COMMON_WORDS = (
+    "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,"
+    "because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,"
+    "for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,"
+    "it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,"
+    "not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,"
+    "since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,"
+    "twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,"
+    "would,yet,you,your").split(',')
 
 def word_list_frequency(word_list):
     """Returns a dictionary of word: frequency for a provided list of words"""
@@ -42,11 +54,15 @@ def chop_text(a_string):
         #Remove '' and split on \n without
 
 
-def top_words(word_list, n=20):
+def top_words(word_list, n=20, ignore_common=True):
     """Returns a sorted list of the n most frequent words in decending order
        Returns list of tuples of ('word', frequency)
     """
     top_words = sorted(word_list.items(), key=lambda x: x[1], reverse=True)
+
+    if ignore_common:
+        top_words = [item for item in top_words if item[0] not in COMMON_WORDS]
+
     return top_words[:n]
 
 
