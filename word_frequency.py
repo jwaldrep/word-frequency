11623c24133bb1748e575e25b831bcd9ebb8fafe
John Waldrep <johnwaldrep@gmail.com>
2015-05-06 12:54:12 -0400

    Add working make_word_list; more rearranging

--- word_frequency.py
+++ word_frequency.py
@@ -10,15 +10,15 @@ def word_frequency(text):
     """Returns a dictionary of word: frequency for a list of words"""
     pass
 
-def make_word_dictionary(filename):
+def make_word_list(filename):
     """Opens filename and returns a list of all words, ignoring case/punctuation
 
     """
-    #read file (context manager)
-    # clean_text on each line
-    #split line into words and add to word_list
-    #return word_list (cleaned)
-    pass
+    word_list = []
+    with open(filename) as my_file:
+        for line  in my_file:
+            word_list.extend(chop_text(line))
+    return word_list
 
 def chop_text(a_string):
     """Takes a string and returns list of lowercased words
@@ -27,12 +27,12 @@ def chop_text(a_string):
     a_string = a_string.lower().strip()
     a_string = re.sub(r'[^A-Za-z ]', '', a_string) #Allow spaces
     return [item for item in a_string.split(' ') if item != ''] #Remove ''
-    
+
 
 def top_words(n = 20):
     """Returns a sorted list of the n most frequent words in decending order"""
     pass
 
 if __name__ == '__main__':
-    #make_word_dictionary('sample.txt')
-    print(chop_text('this is a %#^$&    test \n'))
+    print(make_word_list('short_sample.txt'))
+    #print(chop_text('this is a %#^$&    test \n'))
