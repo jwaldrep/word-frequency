0ba7902265ff50d61ef56c2370e1dc68ad585f13
John Waldrep <johnwaldrep@gmail.com>
2015-05-06 12:17:02 -0400

    Add prototype functions

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,27 @@
 
+#Read text of sample.txt, and return top words with frequency each word occurs
+#    ignore case and punctuation
+#Then ouput 20 most frequent words in descending order
+
+
 def word_frequency(text):
+    """Returns a dictionary of word: frequency for a list of words"""
     pass
+
+def make_word_dictionary(filename):
+    """Opens filename and returns a list of all words, ignoring case/punctuation
+
+    """
+    pass
+
+def clean_text(a_list):
+    """Takes a list of strings and returns lowercase version
+       minus punctuation, whitespace, and line endings
+    """
+    pass
+
+def top_words(n = 20):
+    """Returns a sorted list of the n most frequent words in decending order"""
+    pass
+
+if __name__ == '__main__':
