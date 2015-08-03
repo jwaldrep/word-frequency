cf7d0ad7da084bd47d33f7d01e8c97945be02a9f
John Waldrep <johnwaldrep@gmail.com>
2015-08-03 14:41:12 -0400

    Clean up formatting for PEP8, add missing comments

--- word_frequency.py
+++ word_frequency.py
@@ -16,7 +16,6 @@ COMMON_WORDS = (
 
 def word_list_frequency(word_list):
     """Returns a dictionary of word: frequency for a provided list of words"""
-
     word_frequencies = {}
     for word in word_list:
         word_frequencies[word] = word_frequencies.get(word, 0) + 1
@@ -25,37 +24,38 @@ def word_list_frequency(word_list):
 
 def word_frequency(a_string):
     """Returns frequency of words in a string"""
-
     word_list = chop_text(a_string)
     return word_list_frequency(word_list)
 
+
 def make_word_list(filename):
-    """Opens filename and returns a list of all words, ignoring case/punctuation
-
     """
-
+    Opens filename and returns a list of all words, ignoring case/punctuation
+    """
     word_list = []
     with open(filename) as my_file:
-        for line  in my_file:
+        for line in my_file:
             word_list.extend(chop_text(line))
     return word_list
 
 
 def chop_text(a_string):
-    """Takes a string and returns list of lowercased words
-       minus punctuation, whitespace, and line endings
     """
-
+    Takes a string and returns list of lowercased words
+    minus punctuation, whitespace, and line endings
+    """
     a_string = a_string.lower()
-    a_string = re.sub(r'\n', ' ', a_string) #Replace newlines with spaces
-    a_string = re.sub(r'[^A-Za-z ]', '', a_string) 
-    return [item for item in a_string.split(' ') if item != '']
-        #Remove ''
+    a_string = re.sub(r'\n', ' ', a_string)  # Replace newlines with spaces
+    a_string = re.sub(r'[^A-Za-z ]', '', a_string)
+    return [item for item in a_string.split(' ') if item != ''] # Remove ''
 
 
 def top_words(word_list, n=20, ignore_common=True):
-    """Returns a sorted list of the n most frequent words in decending order
-       Returns list of tuples of ('word', frequency)
+    """
+    Returns a sorted list of the n most frequent words in descending order
+    Returns list of tuples of ('word', frequency)
+
+    Setting ignore_common=True discards words present in COMMON_WORDS
     """
     top_words = sorted(word_list.items(), key=lambda x: x[1], reverse=True)
 
@@ -66,38 +66,47 @@ def top_words(word_list, n=20, ignore_common=True):
 
 
 def display_top_words(word_list, n=20, display_mode='simple'):
+    """
+    Print out the `n` most frequent words in word_list,
+    using the specified display_mode
+
+    if display_mode ==
+    'simple' :  display a list of words and their frequency, descending
+    'histogram' : same as simple, but with bar chart histogram visible
+    'normalized_histogram': same as histogram, but normalized to the max freq
+    """
     top_word_list = top_words(word_list, n)
 
     if display_mode == 'simple':
         for item in top_word_list:
             print('{} {}'.format(*item))
 
-    elif display_mode == 'histogram' or display_mode=='normalized histogram':
+    elif display_mode == 'histogram' or display_mode == 'normalized histogram':
         max_word_length = max([len(item[0]) for item in top_word_list])
         max_freq = top_word_list[0][1]
 
         for my_tuple in top_word_list:
             word, freq = my_tuple
-            padding = ' '*(max_word_length - len(word) + 1)
-            bar = '#' * (freq // 6)  #scale to fit on screen
+            padding = ' ' * (max_word_length - len(word) + 1)
+            bar = '#' * (freq // 6)  # scale to fit on screen
 
             if display_mode == 'normalized histogram':
                 scale_factor = 50 / max_freq
                 bar = '#' * int(freq * scale_factor)
             print('{}{}{} {}'.format(word, padding, bar, freq))
 
-        #print(scale_factor, int(max_freq * scale_factor))
+            # print(scale_factor, int(max_freq * scale_factor))
 
     else:
         print('Invalid display mode selected')
 
 
 if __name__ == '__main__':
-    #print(chop_text('this is a %#^$&    test \n'))
+    # print(chop_text('this is a %#^$&    test \n'))
     word_list = (make_word_list('sample.txt'))
     if len(sys.argv) > 1:
         word_list = make_word_list(sys.argv[1])
     word_dict = (word_list_frequency(word_list))
-    #pprint(top_words(word_dict, n=5 ))
-    #display_top_words(word_dict, n=20, display_mode='simple')
+    # pprint(top_words(word_dict, n=5 ))
+    # display_top_words(word_dict, n=20, display_mode='simple')
     display_top_words(word_dict, n=20, display_mode='normalized histogram')
